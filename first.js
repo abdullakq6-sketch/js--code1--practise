88e60cbc9c19c1ff2676f933c81b5c27209b5122
Abdullah <abdullakq6@gmail.com>
2025-12-11 13:16:55 +0500

    first commit

---
+++ first.js
@@ -0,0 +1,284 @@
+// COMMENT TYPE:
+//1) This is  a code that print 
+// hello world on our console window.
+/* 2)This is  a code that print 
+ hello world on our console window*/
+// console.log("hello world!");
+
+// OPERATORS START:
+// Arithemetic Operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ",a, "& b = ", b);
+// console.log("a + b = ", a+b);
+// console.log("a - b = ", a-b);
+// console.log("a * b = ", a*b);
+// console.log("a / b = ", a/b);
+// console.log("a % b = ", a%b);
+// console.log("a ** b = ", a**b);//5x2=25
+
+//Unary Operator
+// let a = 5;
+// let b = 2;
+// console.log("a = ",a, "& b = ", b);
+// a = a + 1;
+/* + Types:
+ console.log("++a = ", ++a);
+ console.log("a++ = ", a++);
+ console.log("a = ", a);*/
+/*  - Types:
+  console.log("--a = ", --a);
+  console.log("a-- = ", a--);
+ console.log("a = ", a);*/
+// unary operators Type:
+/* 1)a = a +1 //6
+   2)a++; //6
+   3)a = a -1;//4
+   4)a--; //4
+   5)post baad me value change karta he
+   6)pre phele value change karta he
+*/
+// ASIGMENT OPERATORS:
+// let a = 5;
+// let b = 2;
+// FOUR TYPES ASIGMENT OPERATORS:1)+2)-3)*4)/
+//1) a += 4; //a = a + 4
+// console.log("a = ", a);//9
+//2) a -= 4; //a = a - 4
+// console.log("a = ", a);//1
+//3) a *= 4; //a = a * 4
+// console.log("a = ", a);//20
+//4) a /= 4; //a = a / 4
+// console.log("a = ", a);//1.25
+// Comparison OPERATORS:
+// 1) EQUAL TO // ==
+// 2) NOT EQUAL TO //!=
+// let a = 5;
+// let b = 2;
+
+// console.log("5 == 2", a == b)//ANSWER TWO TYPES:1) true 2) False.
+
+// LOGICAL OPERATORS:
+/*  1)true 
+let a = 6;
+let b = 5;
+
+let cond1 = a > b;//true
+let cond2 = a == 6; // true
+console.log("cond1 && cond2 = ", cond1 && cond2);*/
+/*2) false
+let c = 6;
+let d = 5;
+
+let cond3 = c > d;//false
+let cond4 = c == 5; // false
+console.log("cond3 && cond4 = ", cond3 && cond4);*/
+// logical not
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a > b;
+// let cond2 = a == 6; 
+// console.log("!(6 < 5) = ", !(a === 6));
+// OPERATORS END;
+
+// If statement start:
+// CONDITIONAL STATEMENT START:
+// 1) condition checker:
+// let age = 18
+
+// if(age >= 18) {
+//    console.log(" you can vote");
+// }
+
+// if(age < 18) {
+//    console.log(" you CANNOT vote");
+// }
+// ENd
+// 2) condition mode variable checker:
+// let age = 18;
+// let mode = "dark";
+// let color;
+ 
+// if(mode === "dark") {
+//    color = "black";
+// }
+
+// if(mode === "light") {
+//    color = "White";
+// }
+
+// console.log(color);
+// END
+// If statement end;
+
+// If-else statement start:
+//  let mode = "light";
+// let color;
+ 
+// if(mode === "dark") {
+//    color = "black";
+// }  else{
+// color = "white";
+// }
+
+// console.log(color);
+// IF-else end;
+
+// ELSE-IF statement start:
+//  let mode = "dark";
+// let color;
+
+// if(mode === "dark") {
+// color = "black";
+// }  else if(mode === "blue") {
+// color = "blue";
+// } 
+// else if(mode === "pink") {
+// color = "pink";
+// } else {
+//    color = "white";
+// }
+
+// console.log(color);
+
+// if (mode === "dark") {
+// console.log(mode);
+// }
+//  ELSE-IF statement End;
+// Ternary operator start :
+// Adult:
+// let age = 25;
+
+// let result = age >= 18 ? "adult" : "Not adult";
+// console.log(result)
+// Not Adult:
+// let age = 16;
+
+// let result = age >= 18 ? "adult" : "Not adult";
+// console.log(result)
+// Ternary operator END;
+
+// LOGICAL OPERATORS:
+/*  1)true 
+let a = 6;
+let b = 5;
+
+let cond1 = a > b;//true
+let cond2 = a == 6; // true
+console.log("cond1 && cond2 = ", cond1 && cond2);*/
+/*2) false
+let c = 6;
+let d = 5;
+
+let cond3 = c > d;//false
+let cond4 = c == 5; // false
+console.log("cond3 && cond4 = ", cond3 && cond4);*/
+// logical not
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a > b;
+// let cond2 = a == 6; 
+// console.log("!(6 < 5) = ", !(a === 6));
+// OPERATORS END;
+
+
+
+// If statement start:
+// CONDITIONAL STATEMENT START:
+// 1) condition checker:
+// let age = 18
+
+// if(age >= 18) {
+//    console.log(" you can vote");
+// }
+
+// if(age < 18) {
+//    console.log(" you CANNOT vote");
+// }
+// ENd
+// 2) condition mode variable checker:
+// let age = 18;
+// let mode = "dark";
+// let color;
+ 
+// if(mode === "dark") {
+//    color = "black";
+// }
+
+// if(mode === "light") {
+//    color = "White";
+// }
+
+// console.log(color);
+// END
+// If statement end;
+
+
+
+
+// If-else statement start:
+//  let mode = "light";
+// let color;
+ 
+// if(mode === "dark") {
+//    color = "black";
+// }  else{
+// color = "white";
+// }
+
+// console.log(color);
+// IF-else end;
+
+// ELSE-IF statement start:
+//  let mode = "dark";
+// let color;
+
+// if(mode === "dark") {
+// color = "black";
+// }  else if(mode === "blue") {
+// color = "blue";
+// } 
+// else if(mode === "pink") {
+// color = "pink";
+// } else {
+//    color = "white";
+// }
+
+// console.log(color);
+
+// if (mode === "dark") {
+// console.log(mode);
+// }
+//  ELSE-IF statement End;
+
+// Ternary operator start :
+// Adult:
+// let age = 25;
+
+// let result = age >= 18 ? "adult" : "Not adult";
+// console.log(result)
+// Not Adult:
+// let age = 16;
+
+// let result = age >= 18 ? "adult" : "Not adult";
+// console.log(result)
+// Ternary operator END;
+
+
+// ---loops
+// practise Q1:
+// even number code js: for loop
+// for (let i = 0; i <= 100; i++) {
+//       if(i%2 === 1) { //even number
+//        console.log(i)
+//      }
+// }
+// 1st method (odd number code js: for loop)
+// for (let i = 0; i <= 100; i++) {
+//       if(i%2 === 1) { //odd number
+//        console.log(i)
+//      }
+// }
